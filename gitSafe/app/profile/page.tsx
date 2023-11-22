994c2823bdc89496dd35ca431da26c60b554cbad
Elsa <elsa.catoire@apside.com>
2023-11-22 15:55:56 +0100

    first commit

---
+++ gitSafe/app/profile/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+
+export default function Profile() {
+    return (
+        <div className='m-4'>
+            <Button>Click me</Button>
+            <Input />
+        </div>
+    )
+}
