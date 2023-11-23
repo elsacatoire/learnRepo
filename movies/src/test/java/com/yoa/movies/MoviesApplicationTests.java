e9b342f271e9cb6cad4837f5d36ae53df1716633
Elsa <elsa.catoire@apside.com>
2023-11-23 11:38:05 +0100

    initialise project backend for Movies

---
+++ movies/src/test/java/com/yoa/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoa.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
