e5a72984a1e259ed2dd5e052da14e18066803362
Elsa <elsa.catoire@apside.com>
2023-11-23 17:45:55 +0100

    Routing

--- movieClient/movie-gold-v1/src/App.js
+++ movieClient/movie-gold-v1/src/App.js
@@ -3,6 +3,7 @@ import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
 import Layout from './components/Layout';
 import { Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -20,15 +21,17 @@ function App() {
   useEffect(() => {
     getMovies();
   }, [])
+
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={Layout}>
-
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Home />}></Route>
         </Route>
       </Routes>
     </div>
   );
 }
+
 
 export default App;
