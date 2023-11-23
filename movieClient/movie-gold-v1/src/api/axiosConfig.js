7dd84bebe37490fd9d8bae47fb2141fa701a5780
Elsa <elsa.catoire@apside.com>
2023-11-23 17:19:35 +0100

    config requests and fetching data form the back

--- movieClient/movie-gold-v1/src/api/axiosConfig.js
+++ movieClient/movie-gold-v1/src/api/axiosConfig.js
@@ -1,10 +1,10 @@
 // Import the Axios library, which allows making HTTP requests from the browser or Node.js.
 import axios from 'axios';
 
-// Export an instance of Axios configured with a base URL and specific headers. To avoid CORS issues.
+// Export an instance of Axios configured with a base URL and specific headers. To avoid CORS
 export default axios.create({
     // Set the base URL for all requests made with this Axios instance.
-    baseURL: 'https://9c96-103-106-239-104.ap.ngrok.io',
+    baseURL: 'http://localhost:8080',
 
     // Define specific headers that will be included in all requests made with this Axios instance.
     headers: { "ngrok-skip-browser-warning": "true" }
