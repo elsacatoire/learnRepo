fda5d728cf036842427eee1e5441cf4f69e66685
Elsa <elsa.catoire@apside.com>
2023-11-27 17:13:33 +0100

    Theme provider set up

--- gitSafe/app/page.tsx
+++ gitSafe/app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 export default function Home() {
   return (
-    <div>GitSafe</div>
+    <div>
+      <Button>Click me</Button>
+      <Input />
+    </div>
   )
 }
