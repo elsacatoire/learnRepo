d1df5a190b92e805b9daa2b7c368fb189082b13b
Elsa <elsa.catoire@apside.com>
2023-11-28 12:20:14 +0100

    auth github

--- gitSafe/lib/auth.ts
+++ gitSafe/lib/auth.ts
@@ -1,9 +1,11 @@
 import GithubProvider from "next-auth/providers/github"
 import { env } from "./env"
+import { AuthOptions } from "next-auth"
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { prisma } from "./prisma"
 
-
-export const authOptions = {
-    // Configure one or more authentication providers
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
         GithubProvider({
             clientId: env.GITHUB_ID,
--- gitSafe/src/features/layout/Header.tsx
+++ gitSafe/src/features/layout/Header.tsx
@@ -1,12 +1,13 @@
-import { Button } from '@/components/ui/button'
 import { ThemeToggle } from '@/src/theme/ThemeToggle'
 import React from 'react'
+import { LoginButton } from './auth/LoginButton'
 
 export const Header = async () => {
     return (
         <header className='border-b border-b-accent'>
             <div className='container flex flex-item-center py-2 max-w-lg m-auto gap-1'>
                 <h2 className='text-2xl font-bold mr-auto'>GitSafe</h2>
+                <LoginButton />
                 <ThemeToggle />
             </div>
         </header>
