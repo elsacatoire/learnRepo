1a46a56d5b44e5870d6cd836cbf7ddaa0e6cd762
Elsa <elsa.catoire@apside.com>
2023-11-28 11:40:40 +0100

    migration created

---
+++ gitSafe/lib/auth.ts
@@ -0,0 +1,15 @@
+import GithubProvider from "next-auth/providers/github"
+import { env } from "./env"
+
+
+export const authOptions = {
+    // Configure one or more authentication providers
+    providers: [
+        GithubProvider({
+            clientId: env.GITHUB_ID,
+            clientSecret: env.GITHUB_SECRET,
+        }),
+        // ...add more providers here
+    ],
+}
+
