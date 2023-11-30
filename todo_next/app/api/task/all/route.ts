f0bf76d34aa919dd57f33c68a5fe499f780a4e16
Elsa <elsa.catoire@apside.com>
2023-11-30 12:39:56 +0100

    routes get et post

---
+++ todo_next/app/api/task/all/route.ts
@@ -0,0 +1,22 @@
+import Task from "@models/tasks"
+import { connectToDB } from "@src/lib/connect-db"
+import { NextResponse } from "next/server"
+
+export const GET = async (request: Request) => {
+    try {
+        await connectToDB()
+        const tasks = await Task.find({})
+        return NextResponse.json(
+            tasks,
+            { status: 200 }
+        )
+
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json(
+            "Failed to fetch all tasks",
+            { status: 500 }
+        )
+
+    }
+}
