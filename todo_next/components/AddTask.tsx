b10953004489ca14d3f3684fcd777b06691a94ce
Elsa <elsa.catoire@apside.com>
2023-11-30 11:22:00 +0100

    model and schema for data

--- todo_next/components/AddTask.tsx
+++ todo_next/components/AddTask.tsx
@@ -1,11 +1,26 @@
 import { Flex, Input, Button } from "@chakra-ui/react"
 import { SmallAddIcon } from "@chakra-ui/icons"
-import { AddTaskProps } from "@"
+import { AddTaskProps } from "@types"
 
-const AddTask = () => {
+const AddTask = ({ task, setTask, handleCreateTask }: AddTaskProps) => {
     return (
-        <>
-        </>
+        <Flex pt="2rem" pl="2rem" pr="2rem" pb="2rem">
+            <Input
+                placeholder="Nouvelle tâche..."
+                size='lg'
+                onChange={(e) => setTask(e.target.value)}
+                value={task}
+                style={{ 'background': '#fff' }}
+            />
+            <Button
+                colorScheme="twitter"
+                size="lg"
+                onClick={() => handleCreateTask()}
+            >
+                <SmallAddIcon />
+            </Button>
+        </Flex>
+
     )
 }
 
