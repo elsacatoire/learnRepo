ee8ea6322846ebcaff41ed62eb3ff3c83a0146e2
Elsa <elsa.catoire@apside.com>
2023-11-30 09:39:22 +0100

    try fix bug trailer unsuccessfull

---
+++ todo_next/components/AddTask.tsx
@@ -0,0 +1,12 @@
+import { Flex, Input, Button } from "@chakra-ui/react"
+import { SmallAddIcon } from "@chakra-ui/icons"
+import { AddTaskProps } from "@"
+
+const AddTask = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default AddTask
